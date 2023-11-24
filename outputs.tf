5624e60127fe5e63254a3a70eaf827e978304a1e
urixwd <urixwd@gmail.com>
2023-11-24 23:35:29 +0100

    Clean

--- outputs.tf
+++ outputs.tf
@@ -18,19 +18,15 @@ output "aurora_writer_endpoint" {
 output "aurora_reader_endpoint" {
   value = aws_rds_cluster.aurora.reader_endpoint
 }
-
 output "aurora_database_name" {
   value = aws_rds_cluster.aurora.database_name
 }
-
 output "vpc_id" {
   value = aws_vpc.main.id
 }
-
 output "lambda_subnet_ids" {
   value = aws_subnet.private.*.id
 }
-
 output "lambda_security_group_id" {
   value = aws_security_group.lambda.id
 }
